13e1e0e924ddd7e490a1c9a0f9697ab50e17d535
amunwes <56089330+amunwes@users.noreply.github.com>
2023-08-18 21:35:59 -0700

    refactored code a bit, created draw function, removed unnecessary comments

--- main.py
+++ main.py
@@ -16,17 +16,22 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
-width = 8
-height = 8
-margin = 1
+WIDTH = 8
+HEIGHT = 8
+MARGIN = 1
 
-dimx = 80
-dimy = 80
+X_DIM = 80
+Y_DIM = 80
 
-btn_height = 50
+BTN_HEIGHT = 50
 
 
-def update(surface, cur):
+def init_grid(dimx, dimy):
+    cells = np.zeros((dimy, dimx))
+    return cells
+
+
+def update_life(surface, cur):
     nxt = np.zeros((cur.shape[0], cur.shape[1]))
 
     for r, c in np.ndindex(cur.shape):
@@ -41,73 +46,65 @@ def update(surface, cur):
         col = col if cur[r, c] == 1 else col_background
         # pygame.draw.rect(surface, col, (c * sz, r * sz, sz - 1, sz - 1))
         pygame.draw.rect(surface, col,
-                         (c * (width + margin) + margin, r * (width + margin) + margin, width, height))
+                         (c * (WIDTH + MARGIN) + MARGIN, r * (WIDTH + MARGIN) + MARGIN, WIDTH, HEIGHT))
 
     return nxt
 
 
-def init_grid(dimx, dimy):
-    cells = np.zeros((dimy, dimx))
-    return cells
-
-
 def update_grid(r, c, cells):
-    # row = (r - margin) // (width + margin)
-    # col = (c - margin) // (height + margin)
     if cells[r, c] == 1:
         cells[r, c] = 0
     else:
         cells[r, c] = 1
+
     return cells
 
 
 def crawl(cells):
     for r, c in np.ndindex(cells.shape):
         if cells[r, c]:
-            if (0 <= r + 1 < dimx):
+            if (0 <= r + 1 < X_DIM):
                 update_grid(r + 1, c, cells)
 
 
-def main(dimx, dimy, cellsize):
-    pygame.init()
-    surface = pygame.display.set_mode((dimx * cellsize, dimy * cellsize))
-    pygame.display.set_caption("Conway's Game of Life")
-
-    cells = init_grid(dimx, dimy)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-
-        surface.fill(col_grid)
-        cells = update(surface, cells, cellsize)
-        pygame.display.update()
+def draw_grid(grid):
+    for r, c in np.ndindex(grid.shape):
+        if not grid[r, c]:
+            pygame.draw.rect(screen, col_background,
+                             (c * (WIDTH + MARGIN) + MARGIN, r * (WIDTH + MARGIN) + MARGIN, WIDTH, HEIGHT))
+        else:
+            pygame.draw.rect(screen, col_alive,
+                             (c * (WIDTH + MARGIN) + MARGIN, r * (WIDTH + MARGIN) + MARGIN, WIDTH, HEIGHT))
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
     pygame.init()
 
     # Set the width and height of the screen [width, height]
-    board = (margin + dimx * (width + margin), margin + dimy * (height + margin))
-    size = (board[0], board[1] + btn_height)
+    board = (MARGIN + X_DIM * (WIDTH + MARGIN), MARGIN + Y_DIM * (HEIGHT + MARGIN))
+    size = (board[0], board[1] + BTN_HEIGHT)
     screen = pygame.display.set_mode(size)
 
     pygame.display.set_caption("Conway's game of Life")
 
-    grid = init_grid(dimx, dimy)
-    swap_status = init_grid(dimx, dimy)  # 2nd grid to map whether a square has been swapped this click
+    grid = init_grid(X_DIM, Y_DIM)
+    # accompanying grid that tracks whether a cell has been swapped to keep from swapping at the frame rate
+    swap_status = init_grid(X_DIM, Y_DIM)
     mouse_down = False
 
+    # rendering text for buttons
     smallfont = pygame.font.SysFont('sans', 30)
     START = smallfont.render('START', True, WHITE)
     RESET = smallfont.render('RESET', True, WHITE)
-    # grid[1,2] = 1
+    # rendering necessary rectangles
+    btn_bg = pygame.Rect(0, MARGIN + Y_DIM * (HEIGHT + MARGIN), size[0], BTN_HEIGHT)
+    reset_btn = pygame.Rect(0, MARGIN + Y_DIM * (HEIGHT + MARGIN), size[0] / 2, BTN_HEIGHT)
+    start_btn = pygame.Rect(size[0] / 2, MARGIN + Y_DIM * (HEIGHT + MARGIN), size[0] / 2, BTN_HEIGHT)
+    reset_btn.scale_by_ip(x=0.8, y=0.8)
+    start_btn.scale_by_ip(x=0.8, y=0.8)
 
-    # Loop until the user clicks the close button.
+    # start and finish flags for controlling quitting and which loop runs.
     done = False
     start = False
 
@@ -124,71 +121,40 @@ if __name__ == '__main__':
                 mouse_down = True
                 player_position = pygame.mouse.get_pos()
                 if reset_btn.collidepoint(player_position):
-                    grid = init_grid(dimx, dimy)
+                    grid = init_grid(X_DIM, Y_DIM)
                     start = False
                 if start_btn.collidepoint(player_position):
                     start = True
             elif event.type == pygame.MOUSEBUTTONUP:
                 mouse_down = False
-                swap_status = init_grid(dimx, dimy)
+                swap_status = init_grid(X_DIM, Y_DIM)
 
         # --- Game logic should go here
         if mouse_down and not start:
             player_position = pygame.mouse.get_pos()
             if player_position[0] < board[0] and player_position[1] < board[1]:
-                row = (player_position[1] - margin) // (width + margin)
-                col = (player_position[0] - margin) // (height + margin)
+                row = (player_position[1] - MARGIN) // (WIDTH + MARGIN)
+                col = (player_position[0] - MARGIN) // (HEIGHT + MARGIN)
                 if not swap_status[row, col]:
                     swap_status[row, col] = 1
                     grid = update_grid(row, col, grid)
 
-        # crawl(grid)
-
         # --- Screen-clearing code goes here
-
-        # Here, we clear the screen to white. Don't put other drawing commands
-        # above this, or they will be erased with this command.
-
-        # If you want a background image, replace this clear with blit'ing the
-        # background image.
         screen.fill(col_grid)
 
         # --- Drawing code should go here
-        # for r, c in np.ndindex(grid.shape):
-        #     num_alive = np.sum(grid[r - 1:r + 2, c - 1:c + 2]) - grid[r, c]
-        #
-        #     if grid[r, c] == 1 and num_alive < 2 or num_alive > 3:
-        #         col = col_almost_dead
-        #     elif (grid[r, c] == 1 and 2 <= num_alive <= 3) or (grid[r, c] == 0 and num_alive == 3):
-        #         grid[r, c] = 1
-        #         col = col_alive
-        #
-        #     col = col if grid[r, c] == 1 else col_background
-        #     pygame.draw.rect(screen, col,
-        #                  (c * (width + margin) + margin, r * (width + margin) + margin, width, height))
         if start:
-            grid = update(screen, grid)
+            grid = update_life(screen, grid)
         else:
-            for r, c in np.ndindex(grid.shape):
-                if not grid[r, c]:
-                    pygame.draw.rect(screen, col_background,
-                                     (c * (width + margin) + margin, r * (width + margin) + margin, width, height))
-                else:
-                    pygame.draw.rect(screen, col_alive,
-                                     (c * (width + margin) + margin, r * (width + margin) + margin, width, height))
+            draw_grid(grid)
+        # draw the buttons and assign text.
 
-        btn_bg = pygame.Rect(0, margin + dimy * (height + margin), size[0], btn_height)
-        reset_btn = pygame.Rect(0, margin + dimy * (height + margin), size[0] / 2, btn_height)
-        start_btn = pygame.Rect(size[0] / 2, margin + dimy * (height + margin), size[0] / 2, btn_height)
-        reset_btn.scale_by_ip(x=0.8, y=0.8)
-        start_btn.scale_by_ip(x=0.8, y=0.8)
-
-        pygame.draw.rect(screen, col_background, btn_bg)
+        pygame.draw.rect(screen, col_grid, btn_bg)
         pygame.draw.rect(screen, RED, reset_btn)
         pygame.draw.rect(screen, GREEN, start_btn)
 
-        screen.blit(RESET, (reset_btn.topleft[0]+100, reset_btn.topleft[1] ))
-        screen.blit(START, (start_btn.topleft[0]+100, start_btn.topleft[1]) )
+        screen.blit(RESET, (reset_btn.topleft[0] + 100, reset_btn.topleft[1]))
+        screen.blit(START, (start_btn.topleft[0] + 100, start_btn.topleft[1]))
 
         player_position = pygame.mouse.get_pos()
         pygame.draw.circle(screen, RED, (player_position[0], player_position[1]), 2)
